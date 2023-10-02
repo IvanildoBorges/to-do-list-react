296a04b30e5c80758aacb78baa3dce3037f27067
IvanildoBorges <ivanldo.rap@gmail.com>
2023-10-02 17:37:15 -0300

    Adicionado lógica ao componente NewTask

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,23 +1,50 @@
 import { PlusCircle } from "phosphor-react" 
 import { PropsNewTask } from "../models/PropsNewTask"
 import style from "./NewTask.module.css"
+import { 
+    InvalidEvent,
+    ChangeEvent,
+    FormEvent, 
+    useState 
+} from "react"
 
 export function NewTask({ tasksList }: PropsNewTask) {
-    
+    const [task, setTask] = useState(tasksList);
+    const [newTask, setNewTask] = useState("");
+
+    function handleCreateNewTask(event: FormEvent) {
+        event.preventDefault()
+        setTask([...task, newTask])
+        setNewTask("")
+    }
+
+    function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity("")
+        setNewTask(event.target.value)
+    }
+
+    function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+        event.target.setCustomValidity("Por favor digite uma tarefa!")
+    }
+
     return (
         <form 
             className={style.newTask} 
+            onSubmit={handleCreateNewTask}
         >
             <input 
                 type="text" 
                 name="input-task" 
                 id="input-task"
                 placeholder="Adicione uma nova tarefa..." 
+                value={newTask}
+                onChange={handleNewTaskChange}
+                onInvalid={handleNewTaskInvalid}
                 required
             />
             <footer>
                 <button 
-                    disabled 
+                    disabled={newTask.length > 0 ? false : true} 
                     type="submit"
                 >
                     Criar <PlusCircle size={20} />
